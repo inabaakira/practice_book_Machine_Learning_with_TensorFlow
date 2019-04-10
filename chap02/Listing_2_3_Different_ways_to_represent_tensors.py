1d008d3ab4a6e26b1b4e699710fca8bddf10957c
INABA Akira <ainaba@idcf.jp>
2019-04-10 13:52:41 +0900

    [Chap.02] Listing 2.3. Different ways to represent tensors

---
+++ chap02/Listing_2_3_Different_ways_to_represent_tensors.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#-*- mode: python; coding: utf-8 -*-
+# file: Listing_2_3_Different_ways_to_represent_tensors.py
+#    Created:       <2019/04/10 13:42:25>
+#    Last Modified: <2019/04/10 13:47:50>
+
+import tensorflow as tf
+import numpy as np
+
+m1 = [[1.0, 2.0],
+      [3.0, 4.0]]
+
+m2 = np.array([[1.0, 2.0],
+               [3.0, 4.0]], dtype=np.float32)
+
+m3 = tf.constant([[1.0, 2.0],
+                  [3.0, 4.0]])
+
+print(type(m1))
+print(type(m2))
+print(type(m3))
+
+t1 = tf.convert_to_tensor(m1, dtype=tf.float32)
+t2 = tf.convert_to_tensor(m2, dtype=tf.float32)
+t3 = tf.convert_to_tensor(m3, dtype=tf.float32)
+
+print(type(t1))
+print(type(t2))
+print(type(t3))
