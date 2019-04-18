db21677c84bd504f79d1f55c8f0d192441dc3e1b
INABA Akira <inaba.akira@gmail.com>
2019-04-18 21:41:22 +0900

    [Chap.03] Listing 3.2. Solving linear regression

--- chap03/regression.py
+++ chap03/regression.py
@@ -2,13 +2,43 @@
 #-*- mode: python; coding: utf-8 -*-
 # file: regression.py
 #    Created:       <2019/04/18 13:39:50>
-#    Last Modified: <2019/04/18 13:42:28>
+#    Last Modified: <2019/04/18 21:40:26>
 
+import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
+
+learning_rate = 0.01
+training_epochs = 100
 
 x_train = np.linspace(-1, 1, 101)
 y_train = 2 * x_train + np.random.randn(*x_train.shape) * 0.33
 
+X = tf.placeholder(tf.float32)
+Y = tf.placeholder(tf.float32)
+
+def model(X, w):
+    return tf.multiply(X, w)
+
+w = tf.Variable(0.0, name="weights")
+
+y_model = model(X, w)
+cost = tf.square(Y - y_model)
+
+train_op = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+
+sess = tf.Session()
+init = tf.global_variables_initializer()
+sess.run(init)
+
+for epoch in range(training_epochs):
+    for (x, y) in zip(x_train, y_train):
+        sess.run(train_op, feed_dict={X: x, Y: y})
+
+w_val = sess.run(w)
+
+sess.close()
 plt.scatter(x_train, y_train)
-plt.show()
+y_learned = x_train * w_val
+plt.plot(x_train, y_learned, 'r')
+plt.show(    )
