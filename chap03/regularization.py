1dfeed9fef69e241a66b64caa8df074acf0770fa
INABA Akira <inaba.akira@gmail.com>
2019-04-24 20:29:23 +0900

    [Chap.03] Listing 3.5. Evaluating regularization parameters

---
+++ chap03/regularization.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+#-*- mode: python; coding: utf-8 -*-
+# file: polynomial_with_regularization_parameters.py
+#    Created:       <2019/04/24 16:23:27>
+#    Last Modified: <2019/04/27 01:50:44>
+
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+learning_rate = 0.001
+training_epochs = 1000
+reg_lambda = 0.
+
+def split_dataset(x_dataset, y_dataset, ratio):
+    arr = np.arange(x_dataset.size)
+    np.random.shuffle(arr)
+    num_train = int(ratio * x_dataset.size)
+    x_train = x_dataset[arr[0:num_train]]
+    x_test = x_dataset[arr[num_train:x_dataset.size]]
+    y_train = y_dataset[arr[0:num_train]]
+    y_test = y_dataset[arr[num_train:x_dataset.size]]
+    return x_train, x_test, y_train, y_test
+
+x_dataset = np.linspace(-1, 1, 100)
+
+num_coeffs = 9
+y_dataset_params = [0.] * num_coeffs
+y_dataset_params[2] = 1
+y_dataset = 0
+for i in range(num_coeffs):
+    y_dataset += y_dataset_params[i] * np.power(x_dataset, i)
+y_dataset += np.random.randn(*x_dataset.shape) * 0.3
+
+(x_train, x_test, y_train, y_test) = split_dataset(x_dataset, y_dataset, 0.7)
+
+# plt.scatter(x_train, y_train)
+# plt.show()
+
+X = tf.placeholder(tf.float32)
+Y = tf.placeholder(tf.float32)
+
+def model(X, w):
+    terms = []
+    for i in range(num_coeffs):
+        term = tf.multiply(w[i], tf.pow(X, i))
+        terms.append(term)
+    return tf.add_n(terms)
+
+w = tf.Variable([0.] * num_coeffs, name="parameters")
+y_model = model(X, w)
+cost = tf.div(tf.add(tf.reduce_sum(tf.square(Y - y_model)),
+                     tf.multiply(reg_lambda, tf.reduce_sum(tf.square(w)))),
+              2 * x_train.size)
+train_op = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+
+sess = tf.Session()
+init = tf.global_variables_initializer()
+sess.run(init)
+
+for reg_lambda in np.linspace(0, 1, 100):
+    for epoch in range(training_epochs):
+        sess.run(train_op, feed_dict={X: x_train, Y: y_train})
+    final_cost = sess.run(cost, feed_dict={X: x_test, Y: y_test})
+    print('reg lambda', reg_lambda)
+    print('final cost', final_cost)
+
+sess.close()
+
+# for epoch in range(training_epochs):
+#     print("epoch: ", epoch)
+#     sess.run(train_op, feed_dict={X: x_train, Y: y_train})
+# final_cost = sess.run(cost, feed_dict={X: x_test, Y: y_test})
+
+# print('reg lambda', reg_lambda)
+# print('final cost', final_cost)
+
+# w_val = sess.run(w)
+# print(w_val)
+
+# sess.close()
+
+# plt.scatter(x_train, y_train)
+# y_train2 = 0
+# for i in range(num_coeffs):
+#     y_train2 += w_val[i] * np.power(x_train, i)
+
+# x_train_sorted = []
+# y_train2_sorted = []
+# for k, v in sorted(dict(zip(x_train, y_train2)).items()):
+#     x_train_sorted.append(k)
+#     y_train2_sorted.append(v)
+
+# plt.plot(x_train_sorted, y_train2_sorted, 'r')
+# plt.show()
